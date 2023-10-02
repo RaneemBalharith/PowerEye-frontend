6757e52d9b029012ce8f81f65a13179a1e072d84
RaneemBalharith <raneembalharith98@gmail.com>
2023-10-02 16:34:59 +0300

    powereye

--- App.js
+++ App.js
@@ -1,47 +1,12 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList, TouchableOpacity } from 'react-native';
-import Header from './src/components/header';
-import TodoItem from './src/components/todoItem';
-import AddTodo from './src/components/addTodo';
+import { StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
-const [todos, setTodos] = useState([
-  {text: 'buy coffee', key: '1'},
-  {text: 'create an app', key: '2'},
-  {text: 'play on the switch', key: '3'}
-]);
-
-
-const pressHandler = (key) =>{
-  setTodos ((prevTodos) => {
-    return prevTodos.filter(todo => todo.key != key);
-  });
-}
-
-const submitHandler = (text) => {
-  setTodos ((prevTodos)=> {
-    return [
-      {text: text, key: Math.random().toString()},
-      ...prevTodos
-    ]
-  })
-}
-
   return (
     <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        <AddTodo submitHandler={submitHandler}/>
-        <View style={styles.list}>
-            <FlatList
-              data={todos}
-              renderItem={({item}) => (
-                <TodoItem item={item} pressHandler={pressHandler} />
-              )}
-            />
-        </View>
-      </View>
+     <Text>Hi, new app powereye</Text>
     </View>
   );
 }
@@ -50,12 +15,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  content: {
-    padding: 40,
-  },
-  list: {
-    marginTop: 20,
   }
 });
 
