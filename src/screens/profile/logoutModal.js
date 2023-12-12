4ebb538f1bf58a76cb7e697a242a652ca5d660a5
Raneem Balharith <143443822+RaneemBalharith@users.noreply.github.com>
2023-12-12 22:09:20 +0300

    Update logoutModal.js

--- src/screens/profile/logoutModal.js
+++ src/screens/profile/logoutModal.js
@@ -1,18 +1,24 @@
 import React, { useState, useContext } from 'react';
-import { Alert, Modal, StyleSheet, Text, Pressable, View } from 'react-native';
+import { Alert, Modal, StyleSheet, Text, Pressable, View, Platform } from 'react-native';
+import * as Application from 'expo-application';
 import { PowerEyeContext } from '../../services/powerEye.context';
 import { ThemeContext } from "../../services/theme.context";
 import { logoutRequest } from '../../api/apiManager';
 export const LogoutModal = ({ navigation, onModalShow, setOnModalShow }) => {
   const { setToken, token } = useContext(PowerEyeContext)
-  const goLoginScreen = () => {
-    logoutRequest(token).then((res) => {
-      console.log(res)
-    })
-    setToken("")
-    setOnModalShow(!onModalShow)
-  }
-  const { theme } = useContext(ThemeContext)
+  const goLoginScreen = async () => {
+    try {
+      let device_id = Platform.OS === 'ios' ? await Application.getIosIdForVendorAsync() : Application.androidId;
+      await logoutRequest(token, device_id);
+      console.log("Logout successful");
+      console.log("logout id", device_id)
+      setToken("");
+      setOnModalShow(!onModalShow);
+    } catch (error) {
+      console.error("Logout error:", error);
+    }
+  };
+  const { theme } = useContext(ThemeContext);
   return (
     <View style={styles(theme).centeredView}>
       <Modal
